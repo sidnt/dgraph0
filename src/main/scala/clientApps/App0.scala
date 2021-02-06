db8301dd32972d9443b06a702e55611ba7900b89
sidnt <11618157+sidnt@users.noreply.github.com>
2021-02-06 16:05:51 +0530

    [BC] unable to provide layers

--- src/main/scala/clientApps/App0.scala
+++ src/main/scala/clientApps/App0.scala
@@ -2,10 +2,12 @@ package clientApps
 
 import zio._
 import console._
+import clock._
 
 import dgraph0._
-import dgClient._
+import dgHandle._
 import dgConfig._
+import utils._
 
 /** Assuming the database is up:
   * WAP to get DG version and put it out to console
@@ -14,12 +16,18 @@ import dgConfig._
   */
 object App0 extends App:
 
-  val logDgClientVersionZ = for {
-    dgClient <- dgClientAccessor
-    _ <- putStrLn(s"Found Dgraph version ${dgClient.checkVersion.getTag}")
+  val logDgClientVersionZ: ZIO[Console with Clock with DgHandle, Nothing, Unit] = for {
+    dgClient  <- getDgClient
+    _         <- consoleLog(s"Found Dgraph version ${dgClient.checkVersion.getTag}")
   } yield ()
 
+  val l0: ZLayer[Any, Nothing, ZEnv with DgConfig] = defaultDgConfig ++ ZEnv.live
+  val l1: ZLayer[Console with Clock with DgConfig, DgError, DgHandle] = defaultDgHandle
+  val l3: ZLayer[Any, DgError, DgHandle] = l0 >>> l1
+  
   def run(args: List[String]) = 
     logDgClientVersionZ.
-    provideCustomLayer(defaultDgConfig >>> defaultDgClient).
+    provideCustomLayer(l3).
+    provideLayer(ZEnv.live).
     exitCode
+    // provideLayer(defaultDgConfig ++ ZEnv.live >>> defaultDgHandle).exitCode
