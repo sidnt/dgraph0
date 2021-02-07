35055f5eb20c8a3e16a2ef48aef2483c3a8af8ec
sidnt <11618157+sidnt@users.noreply.github.com>
2021-02-07 14:01:20 +0530

    some basic error handling. console error log is lesser.

--- src/main/scala/clientApps/App0.scala
+++ src/main/scala/clientApps/App0.scala
@@ -8,6 +8,7 @@ import dgraph0._
 import dgHandle._
 import dgConfig._
 import utils._
+import DgError._
 
 /** Assuming the database is up:
   * WAP to get DG version and put it out to console
@@ -16,15 +17,20 @@ import utils._
   */
 object App0 extends App:
 
-  val logDgClientVersionZ: ZIO[DgHandle with Console with Clock, Nothing, Unit] = for {
+  val logDgClientVersionZ: ZIO[DgHandle with Console with Clock, DgError, Unit] = for {
     dgClient  <- getDgClient
-    _         <- consoleLog(s"Found Dgraph version ${dgClient.checkVersion.getTag}")
+    _         <- consoleLog(s"domain program begins")
+    dgVersion <- Task(dgClient.checkVersion.getTag) orElse IO.fail(DgErr("getting dgraph version failed. can't communicate to the db probably."))
+    _         <- consoleLog(s"Found Dgraph version ${dgVersion}")
+    _         <- consoleLog(s"domain program ends")
   } yield ()
+
+  val logDgClientVersionHZ = logDgClientVersionZ.catchAll(e => consoleLog(e.toString))
 
   val l0: ZLayer[Any, Nothing, DgConfig] = defaultDgConfig
   val l1: ZLayer[Any, Nothing, DgConfig with Console with Clock] = defaultDgConfig ++ Console.live ++ Clock.live
   val l2: ZLayer[Any, DgError, DgHandle] = l1 >>> defaultDgHandle
   
   def run(args: List[String]) = 
-    logDgClientVersionZ.
+    logDgClientVersionHZ.
     provideCustomLayer(l2).exitCode
