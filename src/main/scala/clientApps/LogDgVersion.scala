6a0b4dd5d7341c67dc76820bc5fd46296102ddca
sidnt <11618157+sidnt@users.noreply.github.com>
2021-02-14 11:36:31 +0530

    rename

--- src/main/scala/clientApps/LogDgVersion.scala
+++ src/main/scala/clientApps/LogDgVersion.scala
@@ -15,22 +15,16 @@ import DgError._
   * .
   * if it is not, the app should put out a one liner saying something went wrong
   */
-object App0 extends App:
+object LogDgVersion extends App:
 
   val logDgClientVersionZ: ZIO[DgHandle with Console with Clock, DgError, Unit] = for {
     dgClient  <- getDgClient
-    _         <- consoleLog(s"domain program begins")
     dgVersion <- Task(dgClient.checkVersion.getTag) orElse IO.fail(DgErr("getting dgraph version failed. can't communicate to the db probably."))
     _         <- consoleLog(s"Found Dgraph version ${dgVersion}")
-    _         <- consoleLog(s"domain program ends")
   } yield ()
 
   val logDgClientVersionHZ = logDgClientVersionZ.catchAll(e => consoleLog(e.toString))
-
-  val l0: ZLayer[Any, Nothing, DgConfig] = defaultDgConfig
-  val l1: ZLayer[Any, Nothing, DgConfig with Console with Clock] = defaultDgConfig ++ Console.live ++ Clock.live
-  val l2: ZLayer[Any, DgError, DgHandle] = l1 >>> defaultDgHandle
   
   def run(args: List[String]) = 
     logDgClientVersionHZ.
-    provideCustomLayer(l2).exitCode
+    provideCustomLayer(layers.l2).exitCode
