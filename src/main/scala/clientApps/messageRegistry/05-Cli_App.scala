61d06ee6965c3ba4ce0b04b3956814c66b536554
sidnt <11618157+sidnt@users.noreply.github.com>
2021-02-16 18:00:48 +0530

    add UpdateMessageByUid functionality

--- src/main/scala/clientApps/messageRegistry/05-Cli_App.scala
+++ src/main/scala/clientApps/messageRegistry/05-Cli_App.scala
@@ -11,9 +11,10 @@ import dgraph0.dgHandle._
 import messageRegistry._
 /**
  * sometimes, we need something as simple as a message registry to get started
- * message registry provides only 2 functionalities
+ * message registry provides only 3 functionalities
  * 1. store a string message
  * 2. query messages based on a string
+ * 3. delete message by uid
  * .
  * the problem with the approach written underneath is,
  * there's a lot of hardcoding, so if we have to extend the functionality of our program
@@ -30,7 +31,7 @@ object Cli extends App {
    * it creates an integer representing the choice of the user */
   val getChoice =
     for {
-      _               <-  putStr("\nenter\n1. to store a string message.\n2. to query messages based on terms.\nAnything else to exit.\nyour choice> ")  
+      _               <-  putStr("\nenter\n1. to store a new string message.\n2. to query messages based on terms.\n3. to update message by id\nAnything else to exit.\nyour choice> ")  
       choice: String  <-  getStrLn.orDie
       choiceInt       <-  Task { choice.toInt } orElse (putStrLn("your choice> exit app") *> UIO(-1))
     } yield choiceInt
@@ -39,6 +40,7 @@ object Cli extends App {
     i match {
       case 1 => StoreUserInputMessage.storeUserInputMessage *> app
       case 2 => QueryMessagesWithTerms.queryMessagesWithTerm *> app
+      case 3 => UpdateMessageByUid.updateMessageByUid0 *> app
       case _ => ZIO.unit
     }
   )
---
+++ src/main/scala/clientApps/messageRegistry/4b-UpdateMessageByuid.scala
@@ -0,0 +1,37 @@
+package clientApps.messageRegistry
+
+import zio._
+import console._
+
+import io.dgraph._
+import DgraphProto._
+
+import com.google.protobuf.ByteString
+
+import dgraph0.dgHandle._
+
+private def makeSetMessageJsonFrom(uid: String, updatedMessage: String): String = {
+  s"""{"uid": "$uid","message": "$updatedMessage"}"""
+  // if there's a problem in this string ^ error will be thrown, eg if "$m" was without quotes
+}
+
+def updateMessageByUid(uid: String, updatedMessage: String) =
+  for {
+    dgClient  <- getDgClient
+    sj        = makeSetMessageJsonFrom(uid, updatedMessage)
+    mu        <- ZIO.effect(Mutation.newBuilder().setSetJson(ByteString.copyFromUtf8(sj)).build) orElse IO.fail(domain.Error("mutation building failed"))
+    txn       = dgClient.newTransaction()  
+    _         <- ZIO.effect{txn.mutate(mu); txn.commit}.ensuring(UIO(txn.discard)) orElse IO.fail(domain.Error("mutation failed"))
+  } yield ()
+
+object UpdateMessageByUid extends App {
+
+  val updateMessageByUid0 = for {
+    uid <- putStrLn("give the correct uid to update> ") *> getStrLn.orDie
+    msg <- putStrLn("give the updated message to store> ") *> getStrLn.orDie
+    _ <- updateMessageByUid(uid, msg).catchAll(de => putStrLn(de.toString))
+  } yield ()
+
+  def run(args: List[String]) = updateMessageByUid0.provideCustomLayer(clientApps.layers.l2).exitCode
+
+}
